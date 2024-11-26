4bff49758a3b59ef8009255a572d4a4c471e3f3f
cambaccarini <cambaccarini01@gmail.com>
2024-11-26 15:55:21 -0300

    navegacion falsa

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from "react";
+import { Routes, Route, Link } from 'react-router-dom'; 
 import TablaRequerimientos from "./TablaRequerimientos";
+import OtraPagina from "./OtraPagina";
 
 function App() {
   return (
-    <div>
-      <TablaRequerimientos />
-    </div>
+      <div>
+        <nav>
+          <ul>
+            <li>
+            <Link to="/tabla-requerimientos">Tabla de Requerimientos</Link>
+            </li>
+            <li>
+            <Link to="/otra-pagina">Otra Página</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+        <Route path="/tabla-requerimientos" element={<TablaRequerimientos />} />
+        <Route path="/otra-pagina" element={<OtraPagina />} />
+      </Routes>
+      </div>
   );
 }
 
---
+++ src/OtraPagina.tsx
@@ -0,0 +1,13 @@
+// OtraPagina.js
+import React from "react";
+
+function OtraPagina() {
+  return (
+    <div>
+      <h1>Esta es otra página</h1>
+      <p>Contenido de la otra página...</p>
+    </div>
+  );
+}
+
+export default OtraPagina;
